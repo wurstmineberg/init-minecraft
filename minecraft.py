e43da64648680f1cc7918457cc55c16ead4126af
Fenhl <fenhl@fenhl.net>
2014-08-19 16:09:32 +0000

    Add automatic version detection

--- minecraft.py
+++ minecraft.py
@@ -15,7 +15,28 @@ Options:
   --version          Print version info and exit.
 """
 
-__version__ = '2.19.0'
+def parse_version_string():
+    path = __file__
+    while os.path.islink(path):
+        path = os.path.join(os.path.dirname(path), os.readlink(path))
+    path = os.path.dirname(path) # go up one level, from repo/minecraft.py to repo, where README.md is located
+    while os.path.islink(path):
+        path = os.path.join(os.path.dirname(path), os.readlink(path))
+    try:
+        version = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], cwd=path).decode('utf-8').strip('\n')
+        if version == 'master':
+            try:
+                with open(os.path.join(path, 'README.md')) as readme:
+                    for line in readme.read().splitlines():
+                        if line.startswith('This is version '):
+                            return line.split(' ')[3]
+            except:
+                pass
+        return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=path).decode('utf-8').strip('\n')
+    except:
+        pass
+
+__version__ = str(parse_version_string())
 
 import sys
 
